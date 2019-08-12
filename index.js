c0f42e3ff62703f55208253868e303baf5d0783d
Shashank Attarde <47862918+SAttarde@users.noreply.github.com>
2019-08-12 23:46:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const { Client, Collection } = require('discord.js');
 const bot = new Client({ disableEveryone: true });
 bot.commands = new Collection();
 ["commands", "events"].forEach(handler => require(`./Handlers/${handler}`)(bot));
-bot.login(process.env.token);
+bot.login(process.env.BOT_TOKEN);
